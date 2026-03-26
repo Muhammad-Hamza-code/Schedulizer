794a031660a11442d73162e4602a907880815de0
Muhammad-Hamza-code <academythetyphon@gmail.com>
2026-03-27 00:12:25 +0500

    config file

--- config.py
+++ config.py
@@ -10,8 +10,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "jZ3j3PfG-23oKAbKKeLv-2vQvNeuFNu1gZs07MoexQY")
 
-from app import db
-db.create_all()
-exit()
-
 
